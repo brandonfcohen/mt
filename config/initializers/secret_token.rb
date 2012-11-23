b8238168f0a299cb55495b68d1cff9c13d971c1d
bakertower <brandonfcohen@gmail.com>
2012-11-23 15:49:54 -0500

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pg::Application.config.secret_token = '32e218478d299fe5ece6dac465dd802c2ef248a7db00f0ef19e79c1344c622cfb3b82ebe11808d727b0c2c78acfbc45372bfe477f677ed8be05274bd23543877'
